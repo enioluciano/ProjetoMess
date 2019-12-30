2ed8532d0d69e00d90a4c5452977e4c8d5ac9850
enioluciano <eniolucianoo@gmail.com>
2019-12-30 16:23:28 -0300

    Botões

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -8,7 +8,20 @@ function Home() {
       <div className="container">
           <div className="borda-redonda">
               <span id='fonte'><strong>YOUR MESSAGE</strong></span>
+              <p id='post'>I will be there in 30 <br/> minutes. I am Sorry</p>
+              
+              <div id='botao1'>
+                  <div id='botao2'>
+
+                  </div>
+
+              </div>
+              
+              
           </div>
+
+          
+
 
       </div>
     
