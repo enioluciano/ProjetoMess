d095cea10bdde5070bc9a01275e8cd9342f4d273
enioluciano <eniolucianoo@gmail.com>
2019-12-30 12:38:24 -0300

    Ajuste

---
+++ src/components/views/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/Tela.css'
+
+
+function Home() {
+    
+  return (
+      <div className="container">
+          <div className="borda-redonda">
+              <span id='fonte'><strong>YOUR MESSAGE</strong></span>
+          </div>
+
+      </div>
+    
+  );
+}
+
+export default Home;
